c08e27755eb19e20dd69b55ceaaf598a6d42277d
Andrei Catalin Anghel <42174110+soitirakis@users.noreply.github.com>
2020-12-01 15:31:44 +0200

    Add files via upload

---
+++ variables2.php
@@ -0,0 +1,24 @@
+<?php
+//Carl
+$Carl_life = rand(65,95);
+
+$Carl_power_array = ["nume" => "Carl", "putere_min" => 60, "putere_max" => 70, "aparare_min" => 40, "aparare_max" => 50,
+"viteza_min" => 40, "viteza_max" => 50, "noroc_min" => 10, "noroc_max" => 30]; //array of random values to use
+
+//Carl extra powers
+$Carl_forta_dragonului_luck = 10; //set the probaility to use this power 10%
+$Carl_scutul_fermecat_luck = 20; //set the probability to use this power 20%
+
+$Carl_super_powers = [$Carl_forta_dragonului_luck, $Carl_scutul_fermecat_luck];
+
+
+//Villain
+
+$Villain_life = rand(55,80);
+
+//array of random values to use
+$Villain_power_array = ["nume" => "Villain", "putere_min" => 50, "putere_max" => 80, "aparare_min" => 35, "aparare_max" => 55,
+"viteza_min" => 40, "viteza_max" => 60,"noroc_min" => 25, "noroc_max" => 40];
+
+
+?>
