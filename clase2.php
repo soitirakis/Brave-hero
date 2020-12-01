b73c2855f5738f2fa589425a9968aff1f0423c84
Andrei Catalin Anghel <42174110+soitirakis@users.noreply.github.com>
2020-12-01 15:29:52 +0200

    Update clase2.php

--- clase2.php
+++ clase2.php
@@ -1,72 +1,118 @@
 <?php
-include("variables.php");
+include("variables2.php");
 
 class Hero{
   protected $viata;
+  protected $nume;
   protected $putere;
   protected $aparare;
   protected $viteza;
   protected $noroc;
   protected $noroc_range;
+  protected $today_lucky;
+  protected $x;
 
   function __construct($viata, $array_of_powers){
-    $this->viata = $viata;
-      $this->putere = $array_of_powers[0];
-      $this->aparare = $array_of_powers[1];
-      $this->viteza = $array_of_powers[2];
-      $this->noroc = $array_of_powers[3];
-      $this->noroc_range = $array_of_powers[4];
+    $i = 1;
+    foreach($array_of_powers as $value){
+      $x = 'value'.$i;
+    //  global $$x;
+      //global $x;
+      $this->{$x} = $value;
+      $i++;
+    }
+      $this->viata = $viata;
+      $this->nume = $this->value1;
+      $this->putere = rand($this->value2,$this->value3);
+      $this->aparare = rand($this->value4,$this->value5);
+      $this->viteza = rand($this->value6,$this->value7);
+    //  $this->noroc = rand($this->value8,$this->value9);
+      $this->noroc_range = rand(0,100);
   }
 
-  function getViata(){
-    return $this->viata;
-  }
-
-  function getPutere(){
-    return $this->putere;
-  }
-
-
-  function getAparare(){
-    return $this->aparare;
-  }
-
-
-  function getViteza(){
-    return $this->viteza;
-  }
-
-  function getNoroc(){
-    return $this->noroc;
-  }
-
-  function getNorocRange(){
-    return $this->noroc_range;
+  function getPowers(){
+    return ["nume" => $this->nume, "viata" => $this->viata, "putere" => $this->putere, "aparare" => $this->aparare, "viteza" => $this->viteza, "noroc" => $this->noroc, $this->noroc_range];
   }
 
   function setViata($noua_viata){
-    $viata = $noua_viata;
+     return $this->viata = $noua_viata;
+  }
+
+  function resetPowers(){
+    //$this->__construct();
+    $this->putere = rand($this->value2,$this->value3);
+    $this->aparare = rand($this->value4,$this->value5);
+    $this->viteza = rand($this->value6,$this->value7);
+    $this->noroc = rand($this->value8,$this->value9);
+    $this->noroc_range = rand(0,100);
+  //  return ;
+  }
+  function checkTodaysLuck(){
+    if(in_array($this->noroc_range, range($this->value8, $this->value9)) ){
+      $this->today_lucky = true;
+    }
+    return $this->today_lucky;
   }
 }
 
 class SuperHero extends Hero {
-  function __construct($viata, $array_of_powers){
+  protected $forta_dragonului_usage;
+  protected $scutul_fermecat_usage;
+  protected $forta_dragonului_luck;
+  protected $scutul_fermecat_luck;
+  protected $use_dragon;
+  protected $use_shield;
+  function __construct($viata, $array_of_powers, $super_powers){
       parent::__construct($viata, $array_of_powers);
 
+      $i = 1;
+      foreach($super_powers as $value){
+        $x = 'value'.$i;
+        $$x = $value;
+        $i++;
+      }
+
+      $this->forta_dragonului_usage = rand(0,100);
+      $this->scutul_fermecat_usage = rand(0,100);
+      $this->forta_dragonului_luck = $value1;
+      $this->scutul_fermecat_luck = $value2;
+      $this->use_dragon = false;
+      $this->use_shield = false;
+
   }
-  function forta_dragonului(){
-    return $this->putere * 2;
+  //check probaility to use forta dragonului 10%
+  function checkForDragon(){
+    if($this->forta_dragonului_usage < $this->forta_dragonului_luck){
+      $this->use_dragon = true;
+    }
+    return $this->use_dragon;
   }
 
+  //use forta dragonului
+  function forta_dragonului(){
+    return $this->putere *= 2;
+  }
+
+  //check to use scutul fermecat 20%
+  function checkForShield(){
+    if($this->scutul_fermecat_usage < $this->scutul_fermecat_luck){
+      $this->use_shield = true;
+    }
+    return $this->use_shield;
+  }
+
+  //use scutul fermecat
   function scutul_fermecat($adversary_power){
     return $adversary_power / 2;
   }
+
+  function resetSuperPowers(){
+    $this->forta_dragonului_usage = rand(0,100);
+    $this->scutul_fermecat_usage = rand(0,100);
+  }
 }
 
-$Carl = new SuperHero($Carl_life, $Carl_powers);
-$Villain = new Hero($Villain_life, $Villain_powers);
-//echo $Carl->getViteza();
-
-
+$Carl = new SuperHero($Carl_life, $Carl_power_array, $Carl_super_powers);
+$Villain = new Hero($Villain_life, $Villain_power_array);
 
 ?>
